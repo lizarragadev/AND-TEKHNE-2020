438a0a360f72d83421ddd959fa7bf901c933b211
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-09-25 22:43:41 -0400

    Final changes from Intents to SimpleLists

--- AND_2020_09_Intents/app/src/main/java/com/miramicodigo/intents/MainActivity.kt
+++ AND_2020_09_Intents/app/src/main/java/com/miramicodigo/intents/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.miramicodigo.intents
 
+import android.Manifest
 import android.app.Activity
+import android.app.SearchManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    // extends  (herencia)  :
+    // implements   (implementaciones)   :   ,
 
     val DEVUELVE_DATOS = 555
+    val PERMISO_LLAMADA = 777
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +37,17 @@ class MainActivity : AppCompatActivity() {
         ibDevolverDatos.setOnClickListener {
             devolverDatos()
         }
+
+        ibAbrirMarcado.setOnClickListener(this)
+        ibLlamar.setOnClickListener(this)
+        ibAbrirGoogleMaps.setOnClickListener(this)
+        ibAbrirStreetView.setOnClickListener(this)
+        ibAbrirPaginaWeb.setOnClickListener(this)
+        ibAbrirBuscador.setOnClickListener(this)
+        ibCompartirTexto.setOnClickListener(this)
+        ibCapturaImagen.setOnClickListener(this)
+        ibEnviarEmail.setOnClickListener(this)
+        ibAbrirApp.setOnClickListener(this)
 
     }
 
@@ -53,9 +77,123 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Se canceló la respuesta", Toast.LENGTH_LONG).show()
             }
+        } else {
+            if (requestCode == 111) {
+                if(resultCode == Activity.RESULT_OK) {
+                    val bitmap = data?.extras?.get("data") as Bitmap
+                    ibCapturaImagen.setImageBitmap(bitmap)
+                } else {
+                    Toast.makeText(this, "Se cancelo", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            ibAbrirMarcado.id -> abrirMarcado()
+            ibLlamar.id -> llamar()
+            ibAbrirGoogleMaps.id -> abrirGoogleMaps()
+            ibAbrirStreetView.id -> abrirStreetView()
+            ibAbrirPaginaWeb.id -> abrirPaginaWeb()
+            ibAbrirBuscador.id -> abrirBuscador()
+            ibCompartirTexto.id -> compartirTexto()
+            ibCapturaImagen.id -> canpturarImagen()
+            ibEnviarEmail.id -> enviarEmail()
+            ibAbrirApp.id -> abrirApp()
+        }
+    }
+
+    private fun abrirApp() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.setClassName("com.miramicodigo.activities", "com.miramicodigo.activities.MainActivity")
+        startActivity(intent)
+    }
+
+    private fun enviarEmail() {
+        val TO = arrayOf("lizarraga.dev@gmail.com", "kotlinlapaz@gmail.com")
+        val CC = arrayOf("luisito@outlook.com")
+        val asunto = "Correo importante"
+        val contenido = "Estimados amigos les envio este correo para informarles...."
+
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.type = "text/plain"
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, TO)
+        intent.putExtra(Intent.EXTRA_CC, CC)
+        intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
+        intent.putExtra(Intent.EXTRA_TEXT, contenido)
+
+        startActivity(intent)
+    }
+
+    private fun canpturarImagen() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePicture ->
+            takePicture.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePicture, 111)
+            }
+        }
+    }
+
+    private fun compartirTexto() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Descarga esta aplicacion del siguiente enlace: bla  bla bla")
+        startActivity(intent)
+    }
+
+    private fun abrirBuscador() {
+        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+        intent.putExtra(SearchManager.QUERY, "Android")
+        startActivity(intent)
+    }
+
+    private fun abrirPaginaWeb() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("https://www.google.com")
+        startActivity(intent)
+    }
+
+    private fun abrirStreetView() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("google.streetview:cbll=-16.508355,-68.126270")
+        startActivity(intent)
+    }
+
+    private fun abrirGoogleMaps() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("geo:-16.508355, -68.126270")
+        startActivity(intent)
+    }
+
+    private fun llamar() {
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), PERMISO_LLAMADA)
+        } else {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:77752810")
+            startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            PERMISO_LLAMADA -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    llamar()
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun abrirMarcado() {
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:77752810")
+        startActivity(intent)
+    }
 
 
 }
--- AND_2020_10_ListasSimples/app/src/main/java/com/miramicodigo/listassimples/MainActivity.kt
+++ AND_2020_10_ListasSimples/app/src/main/java/com/miramicodigo/listassimples/MainActivity.kt
@@ -3,12 +3,23 @@ package com.miramicodigo.listassimples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val paises = resources.getStringArray(R.array.paises)
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, paises)
+
+        lvListaDatos.adapter = adaptador
+
+        lvListaDatos.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(this, "Hizo click en ${paises[i]}", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
