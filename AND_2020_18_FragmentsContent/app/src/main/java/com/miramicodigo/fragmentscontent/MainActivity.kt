67c7d6d7eefaeac40cd0be2223da4f88a322e722
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-09 18:17:22 -0400

    Add base code Fragment content

---
+++ AND_2020_18_FragmentsContent/app/src/main/java/com/miramicodigo/fragmentscontent/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.miramicodigo.fragmentscontent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+    }
+
+}
