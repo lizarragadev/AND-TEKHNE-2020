260c34bfe069f2cadd36debf0dc0e8f48761120b
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-09 23:39:28 -0400

    Fragment simple sample

---
+++ AND_2020_17_FragmentsSimple/app/src/main/java/com/miramicodigo/fragmentssimple/BrasilFragment.kt
@@ -0,0 +1,20 @@
+package com.miramicodigo.fragmentssimple
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BrasilFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_brasil, container, false)
+    }
+
+}
--- AND_2020_17_FragmentsSimple/app/src/main/java/com/miramicodigo/fragmentssimple/MainActivity.kt
+++ AND_2020_17_FragmentsSimple/app/src/main/java/com/miramicodigo/fragmentssimple/MainActivity.kt
@@ -2,12 +2,32 @@ package com.miramicodigo.fragmentssimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ibBolivia.setOnClickListener(this)
+        ibParaguay.setOnClickListener(this)
+        ibBrasil.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id) {
+            ibBolivia.id -> showFragment(BoliviaFragment())
+            ibParaguay.id -> showFragment(ParaguayFragment())
+            ibBrasil.id -> showFragment(BrasilFragment())
+        }
+    }
+
+    fun showFragment(frag: Fragment) {
+        val fragManager = supportFragmentManager
+        val fragTransaction = fragManager.beginTransaction()
+        fragTransaction.replace(R.id.frameContent, frag).commit()
     }
 
 }
