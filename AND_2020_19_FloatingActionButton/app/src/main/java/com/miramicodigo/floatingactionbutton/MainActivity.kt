4a1357513621ba34c7e5ae470315765867205e08
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-16 20:35:06 -0400

    Code final examples

--- AND_2020_19_FloatingActionButton/app/src/main/java/com/miramicodigo/floatingactionbutton/MainActivity.kt
+++ AND_2020_19_FloatingActionButton/app/src/main/java/com/miramicodigo/floatingactionbutton/MainActivity.kt
@@ -2,15 +2,56 @@ package com.miramicodigo.floatingactionbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fab_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    var STATUS = false
+
+    lateinit var show_fab_1: Animation
+    lateinit var show_fab_2: Animation
+    lateinit var show_fab_3: Animation
+    lateinit var hide_fab_1: Animation
+    lateinit var hide_fab_2: Animation
+    lateinit var hide_fab_3: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        show_fab_1 = AnimationUtils.loadAnimation(applicationContext, R.anim.fab1_show)
+        show_fab_2 = AnimationUtils.loadAnimation(applicationContext, R.anim.fab2_show)
+        show_fab_3 = AnimationUtils.loadAnimation(applicationContext, R.anim.fab3_show)
+        hide_fab_1 = AnimationUtils.loadAnimation(applicationContext, R.anim.fab1_hide)
+        hide_fab_2= AnimationUtils.loadAnimation(applicationContext, R.anim.fab2_hide)
+        hide_fab_3 = AnimationUtils.loadAnimation(applicationContext, R.anim.fab3_hide)
+
+        fabMain.setOnClickListener {
+            STATUS = if(!STATUS) {
+                expandFAB()
+                true
+            } else {
+                hideFAB()
+                false
+            }
+        }
+
+        fab_1.setOnClickListener {
+            Toast.makeText(applicationContext, "Click en FAB 1", Toast.LENGTH_SHORT).show()
+        }
+
+        fab_2.setOnClickListener {
+            Toast.makeText(applicationContext, "Click en FAB 2", Toast.LENGTH_SHORT).show()
+        }
+
+        fab_3.setOnClickListener {
+            Toast.makeText(applicationContext, "Click en FAB 3", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
@@ -19,21 +60,21 @@ class MainActivity : AppCompatActivity() {
         layoutParams.rightMargin += (fab_1.width * 1.7).toInt()
         layoutParams.bottomMargin += (fab_1.height * 0.25).toInt()
         fab_1.layoutParams = layoutParams
-        //fab_1.startAnimation(show_fab_1)
+        fab_1.startAnimation(show_fab_1)
         fab_1.isClickable = true
 
         val layoutParams2 = fab_2.layoutParams as FrameLayout.LayoutParams
         layoutParams2.rightMargin += (fab_2.width * 1.5).toInt()
         layoutParams2.bottomMargin += (fab_2.height * 1.5).toInt()
         fab_2.layoutParams = layoutParams2
-        //fab_2.startAnimation(show_fab_2)
+        fab_2.startAnimation(show_fab_2)
         fab_2.isClickable = true
 
         val layoutParams3 = fab_3.layoutParams as FrameLayout.LayoutParams
         layoutParams3.rightMargin += (fab_3.width * 0.25).toInt()
         layoutParams3.bottomMargin += (fab_3.height * 1.7).toInt()
         fab_3.layoutParams = layoutParams3
-        //fab_3.startAnimation(show_fab_3)
+        fab_3.startAnimation(show_fab_3)
         fab_3.isClickable = true
     }
 
@@ -42,21 +83,21 @@ class MainActivity : AppCompatActivity() {
         layoutParams.rightMargin -= (fab_1.width * 1.7).toInt()
         layoutParams.bottomMargin -= (fab_1.height * 0.25).toInt()
         fab_1.layoutParams = layoutParams
-        //fab_1.startAnimation(hide_fab_1)
+        fab_1.startAnimation(hide_fab_1)
         fab_1.isClickable = false
 
         val layoutParams2 = fab_2.layoutParams as FrameLayout.LayoutParams
         layoutParams2.rightMargin -= (fab_2.width * 1.5).toInt()
         layoutParams2.bottomMargin -= (fab_2.height * 1.5).toInt()
         fab_2.layoutParams = layoutParams2
-        //fab_2.startAnimation(hide_fab_2)
+        fab_2.startAnimation(hide_fab_2)
         fab_2.isClickable = false
 
         val layoutParams3 = fab_3.layoutParams as FrameLayout.LayoutParams
         layoutParams3.rightMargin -= (fab_3.width * 0.25).toInt()
         layoutParams3.bottomMargin -= (fab_3.height * 1.7).toInt()
         fab_3.layoutParams = layoutParams3
-        //fab_3.startAnimation(hide_fab_3)
+        fab_3.startAnimation(hide_fab_3)
         fab_3.isClickable = false
     }
 }
--- AND_2020_20_Snackbar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.kt
+++ AND_2020_20_Snackbar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.miramicodigo.snackbar
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSnackbarSimple.setOnClickListener {
+            Snackbar.make(coordinatorLayout, "Snackbar Simple", Snackbar.LENGTH_SHORT).show()
+        }
 
+        btnSnackbarConAccion.setOnClickListener {
+            val snackbar = Snackbar.make(coordinatorLayout, "Snackbar con acción", Snackbar.LENGTH_SHORT)
+            snackbar.setAction("Reintentar") {
+                Toast.makeText(applicationContext, "Click en accion de Snackbar", Toast.LENGTH_SHORT).show()
+            }
+            snackbar.show()
+        }
+
+        btnSnackbarPersonalizado.setOnClickListener {
+            val snackbar = Snackbar.make(coordinatorLayout, "Snackbar personalizado", Snackbar.LENGTH_SHORT)
+            snackbar.setAction("Deshacer") {
+                Toast.makeText(applicationContext, "Click en accion de Snackbar", Toast.LENGTH_SHORT).show()
+            }
+            snackbar.setActionTextColor(Color.parseColor("#E040FB"))
+            snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+            snackbar.setBackgroundTint(Color.parseColor("#4CAF50"))
+            snackbar.show()
+        }
     }
 }
