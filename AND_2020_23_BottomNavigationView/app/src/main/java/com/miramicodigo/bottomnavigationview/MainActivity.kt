df18768382e740aec8b693bbda564e5fe19a03d9
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-23 18:43:55 -0400

    base code bnv

--- AND_2020_23_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.kt
+++ AND_2020_23_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.kt
@@ -2,12 +2,36 @@ package com.miramicodigo.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miramicodigo.bottomnavigationview.view.CalculadoraFragment
+import com.miramicodigo.bottomnavigationview.view.PerfilFragment
+import com.miramicodigo.bottomnavigationview.view.RegistroFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var registroFragment: RegistroFragment
+    lateinit var perfilFragment: PerfilFragment
+    lateinit var calculadoraFragment: CalculadoraFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(savedInstanceState != null) {
+            registroFragment = supportFragmentManager.getFragment(savedInstanceState, "registroFragment") as RegistroFragment
+            perfilFragment = supportFragmentManager.getFragment(savedInstanceState, "perfilFragment") as PerfilFragment
+            calculadoraFragment = supportFragmentManager.getFragment(savedInstanceState, "calculadoraFragment") as CalculadoraFragment
+        } else {
+            registroFragment = RegistroFragment()
+            perfilFragment = PerfilFragment()
+            calculadoraFragment = CalculadoraFragment()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        supportFragmentManager.putFragment(outState, "registroFragment", registroFragment)
+        supportFragmentManager.putFragment(outState, "perfilFragment", perfilFragment)
+        supportFragmentManager.putFragment(outState, "calculadoraFragment", calculadoraFragment)
     }
 }
