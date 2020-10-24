65dd7a0f39978f84a20d718be224ea10ba57514f
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-23 22:45:56 -0400

    Final code dialogs

--- AND_2020_26_Dialogos/app/src/main/java/com/miramicodigo/dialogos/MainActivity.kt
+++ AND_2020_26_Dialogos/app/src/main/java/com/miramicodigo/dialogos/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.miramicodigo.dialogos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_dialog.view.*
+
 
 class MainActivity : AppCompatActivity() {
-
-    private var btnCrear: Button? = null
-    private var btnEntrar: Button? = null
-    private var etNombre: EditText? = null
-    private var etContrasenia: EditText? = null
-    private var posicion: Int = 0
-    private var res: String? = null
-    private var cbRecordar: CheckBox? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,28 +37,115 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun crearDialogoSimple(): AlertDialog {
+        // boton positivo (si, aceptar, permitir)
+        // boton negativo (no, rechazar, denegar)
+        // boton neutro (cancelar, cerrar)
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Mi dialogo")
+            .setMessage("Este es el contenido de mi dialogo")
+            .setCancelable(false)
+            .setPositiveButton("Aceptar") { dInt, i ->
+                Toast.makeText(this, "Click en aceptar", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancelar") { dInt, i ->
+                Toast.makeText(this, "Click en cancelar", Toast.LENGTH_SHORT).show()
+            }
+            .setNeutralButton("Cerrar") { dInt, i ->
+                Toast.makeText(this, "Click en cerrar", Toast.LENGTH_SHORT).show()
+            }
 
-        return null!!
+        return alertDialog.create()
     }
 
 
     fun crearDialogoConLista(): AlertDialog {
-
-        return null!!
+        val alertDialog = AlertDialog.Builder(this)
+        val opciones = arrayOf(
+            "Opcion 1",
+            "Opcion 2",
+            "Opcion 3",
+            "Opcion 4",
+            "Opcion 5",
+            "Opcion 6"
+        )
+        alertDialog.setTitle("Elija una opcion")
+            .setCancelable(false)
+            .setItems(opciones) { dInt, i ->
+                Toast.makeText(this, "Hizo click en ${opciones[i]}", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancelar") { dInt, i ->
+                Toast.makeText(this, "Cerrar", Toast.LENGTH_SHORT).show()
+            }
+        return alertDialog.create()
     }
 
     fun crearDialogoConListaRadio(): AlertDialog {
-
-        return null!!
+        val alertDialog = AlertDialog.Builder(this)
+        val opciones = arrayOf("Radio 1", "Radio 2", "Radio 3", "Radio 4", "Radio 5", "Radio 6")
+        var posicion = 0
+        alertDialog.setTitle("Elija una opcion del radio")
+            .setCancelable(false)
+            .setSingleChoiceItems(opciones, 0) { dInt, i ->
+                posicion = i
+            }
+            .setPositiveButton("Aceptar") { dInt, i ->
+                Toast.makeText(
+                    this,
+                    "Selecciono la opcion ${opciones[posicion]}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setNegativeButton("Cancelar") { dInt, i ->
+                Toast.makeText(this, "Cerrar", Toast.LENGTH_SHORT).show()
+            }
+        return alertDialog.create()
     }
 
     fun crearDialogoCheckBox(): AlertDialog {
-
-        return null!!
+        val alertDialog = AlertDialog.Builder(this)
+        val opciones = arrayOf("Android Dev", "iOS Dev", "Python Dev", "Go Dev", "ML Dev")
+        val itemSeleccionados = ArrayList<Int>()
+        alertDialog.setTitle("Elige tus skills")
+            .setCancelable(false)
+            .setMultiChoiceItems(opciones, null) { dInt, i, b ->
+                if(b) {
+                    itemSeleccionados.add(i)
+                } else {
+                    if(itemSeleccionados.contains(i)) {
+                        itemSeleccionados.remove(i)
+                    }
+                }
+            }
+            .setPositiveButton("Aceptar") { dInt, i ->
+                var res = ""
+                itemSeleccionados.forEach {
+                    res = res + "\n ${opciones[it]}"
+                }
+                Toast.makeText(this, "Seleccionados $res", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancelar") { dInt, i ->
+                Toast.makeText(this, "Cerrar", Toast.LENGTH_SHORT).show()
+            }
+        return alertDialog.create()
     }
 
-    fun crearDialogoPersonalizado(): AlertDialog {
+    fun crearDialogoPersonalizado() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setCancelable(false)
+        val v = layoutInflater.inflate(R.layout.custom_dialog, null)
+        alertDialog.setView(v)
 
-        return null!!
+        val dialog = alertDialog.show()
+
+        v.btnEntrar.setOnClickListener {
+            Toast.makeText(this, "Nombre: ${v.etNombre.text}", Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+        }
+
+        v.btnCrear.setOnClickListener {
+            dialog.dismiss()
+        }
+
     }
+
 }
