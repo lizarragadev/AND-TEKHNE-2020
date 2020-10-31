ad97ae571e115473f4b422d7e286366ff3000f40
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-10-30 23:07:32 -0400

    Final code SharedPreferences

--- AND_2020_30_SharedPreferences/app/src/main/java/com/miramicodigo/sharedpreferences/MainActivity.kt
+++ AND_2020_30_SharedPreferences/app/src/main/java/com/miramicodigo/sharedpreferences/MainActivity.kt
@@ -6,14 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    // Almacenamiento en Android
+    // 1. SharedPreferences (Local)  "*.xml" (Informacion pequeña)
+    // 2. Archivos (Local) ".txt" (Almacenamiento interno y Almacenamiento externo)  (Información extensa)
+    // 3. Base de datos (Local) (Informacion estructurada)
+
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sharedPreferences = getSharedPreferences("mis_preferencias", Context.MODE_PRIVATE)
+        // { clave: valor }
+
+        if(!sharedPreferences.getBoolean("primeraVez", false)) {
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("primeraVez", true)
+            editor.apply()
+            etLabelInit.text = "Es primera vez que ingresas"
+        } else {
+            etLabelInit.text = "Ya ingresaste mas de 1 vez"
+        }
 
         btnGuardarUno.setOnClickListener(this)
         btnGuardarDos.setOnClickListener(this)
@@ -31,10 +50,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     fun guardarValor(texto: String, nombre: String, et: EditText) {
-
+        val editor = sharedPreferences.edit()
+        editor.putString(nombre, texto)
+        editor.apply()
+        et.setText("")
+        Toast.makeText(this, "Se guardo existosamente", Toast.LENGTH_SHORT).show()
     }
 
     fun leerValor(nombre: String): String {
-        return ""
+        return sharedPreferences.getString(nombre, "").toString()
     }
 }
