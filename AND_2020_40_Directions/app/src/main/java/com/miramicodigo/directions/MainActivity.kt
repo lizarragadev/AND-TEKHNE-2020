464e02be3be34b7c2e92f8ab931603203d73e0e6
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-04 23:11:53 -0400

    Add final code directions api demo

--- AND_2020_40_Directions/app/src/main/java/com/miramicodigo/directions/MainActivity.kt
+++ AND_2020_40_Directions/app/src/main/java/com/miramicodigo/directions/MainActivity.kt
@@ -25,6 +25,8 @@ import java.util.concurrent.TimeUnit
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     lateinit var googleMap: GoogleMap
+    val inicioLatLng = LatLng(-16.504413, -68.131360)
+    val finLatLng = LatLng(-16.495258, -68.128334)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +35,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         title = "Demo Directions API"
 
         initMap()
-
     }
 
     private fun initMap() {
@@ -42,7 +43,81 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(gmap: GoogleMap?) {
+        if(gmap != null)
+            googleMap = gmap
+
+        setUpBounds()
+        getDirectionsDetails(inicioLatLng, finLatLng, TravelMode.WALKING)
+    }
+
+    private fun setUpBounds() {
+        googleMap.setOnMapLoadedCallback {
+            runOnUiThread {
+                val builder = LatLngBounds.Builder()
+                builder.include(inicioLatLng.toAndroidLatLng())
+                builder.include(finLatLng.toAndroidLatLng())
+                val llBounds = builder.build()
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(llBounds, 50))
+            }
+        }
+    }
+
+    private fun LatLng.toAndroidLatLng() : com.google.android.gms.maps.model.LatLng {
+        return com.google.android.gms.maps.model.LatLng(this.lat, this.lng)
+    }
+
+    private fun getDirectionsDetails(origen: LatLng, destino: LatLng, modo: TravelMode) {
+        val hora = DateTime()
+        try {
+            val req = DirectionsApi.newRequest(geoApiContext())
+                    .mode(modo)
+                    .origin(origen)
+                    .destination(destino)
+                    .departureTime(hora)
+                    //.waypoints(LatLng(-16.495281, -68.136717))
+
+            req.setCallback(object: PendingResult.Callback<DirectionsResult> {
+                override fun onFailure(e: Throwable?) {
+
+                }
+
+                override fun onResult(result: DirectionsResult?) {
+                    runOnUiThread {
+                        if(result != null)
+                            addPolyline(result)
+                    }
+                }
+            })
+
+        } catch (e: ApiException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun addPolyline(results: DirectionsResult) {
+        val route = results.routes[0].legs[0]
+        val duration = route.duration
+        tvTiempoEstimado.text = duration.humanReadable
+        results.routes
+                .map {
+                    PolyUtil.decode(it.overviewPolyline.encodedPath)
+                }.map {
+                    googleMap.addPolyline(PolylineOptions().addAll(it))
+                }.forEach {
+                    it.color = ActivityCompat.getColor(applicationContext, R.color.colorLine)
+                }
 
     }
+
+    private fun geoApiContext(): GeoApiContext{
+        val geoApiContext = GeoApiContext()
+        return geoApiContext
+                .setQueryRateLimit(3)
+                .setApiKey(getString(R.string.api_key))
+                .setConnectTimeout(1500, TimeUnit.MILLISECONDS)
+                .setReadTimeout(1500, TimeUnit.MILLISECONDS)
+                .setWriteTimeout(1500, TimeUnit.MILLISECONDS)
+    }
+
 
 }
