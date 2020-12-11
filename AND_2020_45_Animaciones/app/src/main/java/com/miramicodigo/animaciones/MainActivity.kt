7d5b5d0045075d56a71f276c8d58daad94cd1b84
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-11 18:11:15 -0400

    add base code animations

---
+++ AND_2020_45_Animaciones/app/src/main/java/com/miramicodigo/animaciones/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.miramicodigo.animaciones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var animacion: Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnZoomin.setOnClickListener { zoomin() }
+        btnZoomout.setOnClickListener { zoomout() }
+        btnFadein.setOnClickListener { fadein() }
+        btnFadeout.setOnClickListener { fadeout() }
+        btnRotate.setOnClickListener { rotate() }
+        btnMove.setOnClickListener { move() }
+        btnSlideUp.setOnClickListener { slideup() }
+        btnSlidedown.setOnClickListener { slidedown() }
+        btnBlink.setOnClickListener { blink() }
+        btnBounce.setOnClickListener { bounce() }
+        btnCrossfade.setOnClickListener { crossfade() }
+        btnSequential.setOnClickListener { sequential() }
+        btnTogether.setOnClickListener { together() }
+        btnStop.setOnClickListener { stop() }
+    }
+
+    fun zoomin() {
+
+    }
+
+    fun zoomout() {
+
+    }
+
+    fun fadein() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun fadeout() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun rotate() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun move() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.move)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun slideup() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun slidedown() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.slide_down)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun blink() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.blink)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun bounce() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.bounce)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun crossfade() {
+
+    }
+
+    fun sequential() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.sequential)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun together() {
+        animacion = AnimationUtils.loadAnimation(this, R.anim.together)
+        ivOctocat.startAnimation(animacion)
+    }
+
+    fun stop() {
+
+    }
+}
