9089e42d53c813090a442b793e8d6c435e788ae7
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-11 16:16:50 -0400

    Add base code MediaPlayer

---
+++ AND_2020_43_MediaPlayer/app/src/main/java/com/miramicodigo/mediaplayer/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.miramicodigo.mediaplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
