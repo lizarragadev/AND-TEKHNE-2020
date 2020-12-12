b351f7c3184ad472776a3e4131911659faea6c51
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-12 00:45:04 -0400

    SoundPool demo final code

--- AND_2020_44_SoundPool/app/src/main/java/com/miramicodigo/soundpool/MainActivity.kt
+++ AND_2020_44_SoundPool/app/src/main/java/com/miramicodigo/soundpool/MainActivity.kt
@@ -1,16 +1,64 @@
 package com.miramicodigo.soundpool
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spIdiota: SoundPool
+    lateinit var spCallate: SoundPool
+    lateinit var spVeteAlDiablo: SoundPool
+
+    var resIdiota = 0
+    var resCallate = 0
+    var resVeteAlDiablo = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+        createSoundPool()
 
+        ivIdiota.setOnClickListener { idiota() }
+        ivCallate.setOnClickListener { callate() }
+        ivVeteAlDiablo.setOnClickListener { veteAlDiablo() }
+    }
+
+    private fun createSoundPool() {
+        val attributes = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).build()
+        spIdiota = SoundPool.Builder().setAudioAttributes(attributes).build()
+        spCallate = SoundPool.Builder().setAudioAttributes(attributes).build()
+        spVeteAlDiablo = SoundPool.Builder().setAudioAttributes(attributes).build()
+        loadSoundPool()
+    }
+
+    private fun loadSoundPool() {
+        resIdiota = spIdiota.load(this, R.raw.idiota, 1)
+        resCallate = spCallate.load(this, R.raw.callate, 1)
+        resVeteAlDiablo = spVeteAlDiablo.load(this, R.raw.vete_al_diablo, 1)
+    }
+
+    private fun idiota() {
+        if(resIdiota != 0) {
+            spIdiota.play(resIdiota, 1.0f, 1.0f, 0, 0, 0f)
+        }
+    }
+
+    private fun callate() {
+        if(resCallate != 0) {
+            spCallate.play(resCallate, 1.0f, 1.0f, 0, 0, 0f)
+        }
+    }
+
+    private fun veteAlDiablo() {
+        if(resVeteAlDiablo != 0) {
+            spVeteAlDiablo.play(resVeteAlDiablo, 1.0f, 1.0f, 0, 0, 0f)
+        }
     }
 
 }
