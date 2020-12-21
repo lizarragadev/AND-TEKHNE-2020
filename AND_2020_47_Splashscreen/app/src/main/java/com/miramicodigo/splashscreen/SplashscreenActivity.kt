a04b3f34b9d73eb71f9d947caa427e10248a1a7f
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-21 18:36:07 -0400

    Add base code Splashscreen

---
+++ AND_2020_47_Splashscreen/app/src/main/java/com/miramicodigo/splashscreen/SplashscreenActivity.kt
@@ -0,0 +1,14 @@
+package com.miramicodigo.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        supportActionBar?.hide()
+
+
+    }
+}
