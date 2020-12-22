01ad6997aad0055545b62c9477241fb987809f11
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-12-22 01:30:56 -0400

    add final code splashscreen

--- AND_2020_47_Splashscreen/app/src/main/java/com/miramicodigo/splashscreen/SplashscreenActivity.kt
+++ AND_2020_47_Splashscreen/app/src/main/java/com/miramicodigo/splashscreen/SplashscreenActivity.kt
@@ -1,7 +1,11 @@
 package com.miramicodigo.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splashscreen.*
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +13,22 @@ class SplashscreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splashscreen)
         supportActionBar?.hide()
 
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        ivLogo.startAnimation(animacion)
 
+        animacion.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+        })
     }
 }
